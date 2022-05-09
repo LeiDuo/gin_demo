f91bfa6abd7af1422de5b1c665b8af85f99cf7c5
LeiDuo <1242853262@qq.com>
2022-05-09 17:32:27 +0800

    init

--- http_test.go
+++ http_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"testing"
 )
 
 // http.ResponseWriter：代表响应，传递到前端的
 // *http.Request：表示请求，从前端传递过来的
-func sayHello(w http.ResponseWriter, r *http.Request) {
+func sayHello(w http.ResponseWriter, _ *http.Request) {
 	_, _ = fmt.Fprintln(w, "<h1 style='color:red'>hello Golang!<h1>")
 }
 
@@ -19,4 +21,53 @@ func TestHttp(b *testing.T) {
 		fmt.Printf("http server failed, err:%v \n", err)
 		return
 	}
+}
+
+func TestUpload(t *testing.T) {
+	router := gin.Default()
+	// 处理multipart forms提交文件时默认的内存限制是32 MiB
+	// 可以通过下面的方式修改
+	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// 单个文件
+		file, err := c.FormFile("f1")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		log.Println(file.Filename)
+		dst := fmt.Sprintf("./download/%s", file.Filename)
+		// 上传文件到指定的目录
+		_ = c.SaveUploadedFile(file, dst)
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("'%s' uploaded!", file.Filename),
+		})
+	})
+	_ = router.Run(":9090")
+}
+
+func TestMultiUpload(t *testing.T) {
+	router := gin.Default()
+	// 处理multipart forms提交文件时默认的内存限制是32 MiB
+	// 可以通过下面的方式修改
+	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	router.POST("/multi_upload", func(c *gin.Context) {
+		// Multipart form
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+
+		for index, file := range files {
+			log.Println(file.Filename)
+			dst := fmt.Sprintf("./download/%s_%d", file.Filename, index)
+			// 上传文件到指定的目录
+			_ = c.SaveUploadedFile(file, dst)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("%d files uploaded!", len(files)),
+		})
+	})
+	_ = router.Run(":9090")
 }
